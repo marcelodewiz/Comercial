1561fb0da12a417b3064848fe09289d616556c12
Marcelo Migotto <marcelo.dewiz@gmail.com>
2024-03-10 20:14:25 -0300

    Criação da entidade Cliente

---
+++ Comercial.Shared.Dados/Banco/ComercialContext.cs
@@ -0,0 +1,18 @@
+﻿using Comercial.Shared.Modelos.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comercial.Shared.Dados.Banco;
+
+public class ComercialContext : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Comercial;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+}
---
+++ Comercial.Shared.Modelos/Modelos/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Comercial.Shared.Modelos.Modelos;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email {  get; set; }
+    public string CPF { get; set; }
+    public string RG { get; set; }
+
+}

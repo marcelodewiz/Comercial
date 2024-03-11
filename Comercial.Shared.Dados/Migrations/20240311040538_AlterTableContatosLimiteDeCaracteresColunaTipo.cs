7bd9e0604dc1486b3a9b4e5e0ccc44e6461218e3
Marcelo Migotto <marcelo.dewiz@gmail.com>
2024-03-11 01:06:38 -0300

    Refatoração no limite de caracteres da coluna tipo na tabela contatos

---
+++ Comercial.Shared.Dados/Migrations/20240311040538_AlterTableContatosLimiteDeCaracteresColunaTipo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comercial.Shared.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableContatosLimiteDeCaracteresColunaTipo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Contatos",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
--- Comercial.Shared.Modelos/Modelos/Contato.cs
+++ Comercial.Shared.Modelos/Modelos/Contato.cs
@@ -6,15 +6,11 @@ namespace Comercial.Shared.Modelos.Modelos;
 public class Contato
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "O campo Tipo é obrigatório.")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "O campo Tipo deve conter apenas letras.")]
+    [MaxLength(10)]
     public string Tipo { get; set; }
-    [Required(ErrorMessage = "O campo DDD é obrigatório.")]
     public int DDD { get; set; }
     [Column(TypeName = "decimal(9,0)")]
-    [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
     public decimal Telefone { get; set; }
-    [Required(ErrorMessage = "O campo Cliente é obrigatório.")]
     public virtual Cliente Cliente { get; set; }
 
 }
